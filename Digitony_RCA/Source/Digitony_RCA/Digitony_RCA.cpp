5b1ebcd7cb124fcc2d7027f46ebaf1492ef9a1f8
hyeona3241 <pha2168@gmail.com>
2024-07-11 14:44:35 +0900

    BaseProject
    2024.07.11

---
+++ Digitony_RCA/Source/Digitony_RCA/Digitony_RCA.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Digitony_RCA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Digitony_RCA, "Digitony_RCA" );
